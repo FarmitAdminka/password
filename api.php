45e135e4df2b894abacd6a68d647d75e8e901f44
FarmitAdminka <127196586+FarmitAdminka@users.noreply.github.com>
2024-10-14 09:04:02 +0300

    Update api.php

--- api.php
+++ api.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 use phpseclib\Crypt\RSA;
 require_once __DIR__ . '/../vendor/autoload.php';
+echo json_encode(['status' => 'OK', 'received_password' => $_POST['password'] ?? 'none']);
 
 error_log("POST данные: " . print_r($_POST, true), 3, "/tmp/mylog.log");
 
